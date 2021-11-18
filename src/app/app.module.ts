b485f1e3b4730ee36a1ccaf1e0049cbbe576cc78
Kadeem Best <kadeem.best@theweekendhack.com>
2021-11-17 22:05:17 -0500

    Home Page Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { ResortItemComponent } from './resort-item/resort-item.component';
+import { ResortListComponent } from './resort-list/resort-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   HeaderComponent,
+    FooterComponent,
+    HeroComponent, 
+    ResortItemComponent,
+    ResortListComponent
   ],
   imports: [
     BrowserModule,
